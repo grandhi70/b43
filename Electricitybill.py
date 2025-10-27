ba0173ec5a9ce80be1a16fba45029fbbc8256a92
grandhi70 <grandhi.srini@gmail.com>
2025-10-27 11:16:12 -0500

    my first commit

---
+++ Electricitybill.py
@@ -0,0 +1,27 @@
+# Electricity bill
+import sys
+PastReading = int(sys.argv[1])
+PresentReading = int(sys.argv[2])
+Total_units = PresentReading - PastReading
+print("Total Units:", Total_units)
+
+eBill = 0
+
+if Total_units > 500 :
+    eBill = Total_units * 10
+    print(f"Your electricity Bill is: Rs.{eBill}")
+elif Total_units > 400 and Total_units <= 500:
+    eBill = Total_units * 8
+    print(f"Your electricity Bill is: Rs.{eBill}")
+elif Total_units > 300 and Total_units <= 400:
+    eBill = Total_units * 6
+    print(f"Your electricity Bill is: Rs.{eBill}")
+elif Total_units > 200 and Total_units <= 300:
+    eBill = Total_units * 4
+    print(f"Your electricity Bill is: Rs.{eBill}")
+elif Total_units >= 0 and Total_units <= 200:
+    eBill = Total_units * 2
+    print(f"Your electricity Bill is: Rs.{eBill}")
+
+
+    
